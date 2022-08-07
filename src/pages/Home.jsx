535de5c8298be3564d1a7be8dfb15fdb7c88bd0d
shahin <shahin.shahi1986@gmail.com>
2022-08-07 11:47:34 +0300

    upload date picker

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import bg from "../images/frontpagebg.png"
 import logo from "../images/airbnb.png"
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, Select, DatePicker } from "web3uikit";
 
 
 const Home = () => {
@@ -28,6 +28,52 @@ const Home = () => {
       </div>
       <div className="tabContent">
         <div className="searchFields">
+          <div className="inputs">
+            Location
+            <Select
+              defaultOptionIndex={0}
+              onChange={(data) => console.log(data.label)}
+              options={[
+                {
+                  id: "ny",
+                  label: "New York"
+                },
+
+                {
+                  id: "lon",
+                  label: "London"
+                },
+                {
+                  id: "db",
+                  label: "Dubai"
+                },
+                {
+                  id: "la",
+                  label: "Los angles"
+                },
+              ]}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Check In
+            <DatePicker
+              id="checkIn"
+              onChange={(event)=>console.log(event.checkIn)}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Check Out
+            <DatePicker
+              id="checkOut"
+              onChange={(event)=>console.log(event.checkOut)}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Gests
+          </div>
 
         </div>
       </div>
