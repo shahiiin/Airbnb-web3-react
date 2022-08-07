9b6ccb34d8f2c7da8b086009fd4dfb453f9faa67
shahin <shahin.shahi1986@gmail.com>
2022-08-07 19:14:58 +0300

    make api from google maps

--- src/pages/Rentals.jsx
+++ src/pages/Rentals.jsx
@@ -1,31 +1,39 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Rentals.css"
 import { Link } from "react-router-dom";
 import logo from "../images/airbnbRed.png"
-import { ConnectButton, Icon,Button } from "web3uikit";
+import { ConnectButton, Icon, Button } from "web3uikit";
 import { useLocation } from "react-router";
+import RentalsMap from '../components/RentalsMap'
 
 const Rentals = () => {
 
   const { state: searchFilters } = useLocation();
   const [highLight, setHighLight] = useState();
   const rentalsList = [
-    
-      {
-        attributes: {
-          city: "New York",
-          unoDescription: "3 Guests • 2 Beds • 2 Rooms",
-          dosDescription: "Wifi • Kitchen • Living Area",
-          imgUrl:
-            "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
-          lat: "40.716862",
-          long: "-73.999005",
-          name: "Apartment in China Town",
-          pricePerDay: "3",
-        },
+
+    {
+      attributes: {
+        city: "New York",
+        unoDescription: "3 Guests • 2 Beds • 2 Rooms",
+        dosDescription: "Wifi • Kitchen • Living Area",
+        imgUrl:
+          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
+        lat: "40.716862",
+        long: "-73.999005",
+        name: "Apartment in China Town",
+        pricePerDay: "3",
       },
-    
+    },
   ]
+
+  let cords = [];
+  rentalsList.forEach((item) => {
+    cords.push({ lat: item.attributes.lat, lng: item.attributes.long });
+  });
+
+
+
   return (
     <>
 
@@ -74,15 +82,15 @@ const Rentals = () => {
         <div className="rentalsContentL">
           Stay Available for your Destinations
           {rentalsList &&
-            rentalsList.map((item,index) => {
-           
+            rentalsList.map((item, index) => {
+
               return (
                 <>
-                 <hr className="line2" />
+                  <hr className="line2" />
                   <div key={index} className={highLight == index ? "rentalDivH " : "rentalDiv"}>
                     <img className="rentalImg"
-                     src={item.attributes.imgUrl}
-                      alt="img"/>
+                      src={item.attributes.imgUrl}
+                      alt="img" />
                     <div className="rentalInfo">
                       <div className="rentalTitle">
                         {item.attributes.name}
@@ -94,9 +102,9 @@ const Rentals = () => {
                         {item.attributes.dosDescription}
                       </div>
                       <div className="bottomButton">
-                        <Button 
-                        
-                        text="Stay Here" />
+                        <Button
+
+                          text="Stay Here" />
                         <div className="price">
                           <Icon fill="#808080" size={10} svg="matic" />
                           {item.attributes.pricePerDay} / Day
@@ -105,11 +113,12 @@ const Rentals = () => {
                     </div>
                   </div>
                 </>
-            )
+              )
             })
           }
         </div>
         <div className="rentalsContentR">
+          <RentalsMap locations={cords}/>
         </div>
       </div>
     </>
