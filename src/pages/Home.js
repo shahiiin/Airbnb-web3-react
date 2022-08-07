29979b8ea07efabb53a02ccf65046d7dcdf8f907
shahin <shahin.shahi1986@gmail.com>
2022-08-07 10:22:24 +0300

    upload project

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import bg from "../images/frontpagebg.png"
+import logo from "../images/airbnb.png"
+
+
+
+const Home = () => {
+
+  return (
+    <div className="container" style={{backgroundImage:`url(${bg})`}}>
+ 
+    </div>
+  );
+};
+
+export default Home;
